1c35b3f1d561b5351d34fe3219670731d5dde74d
sung uk <sunguk0204@monkey-soft.com>
2022-03-07 17:39:26 +0900

    First commit

---
+++ screens/WriteScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import WriteEditor from '../components/WriteEditor';
+import WriteHeader from '../components/WriteHeader';
+
+function WriteScreen() {
+  return (
+    <SafeAreaView style={styles.block}>
+      <WriteHeader />
+      <WriteEditor />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+
+export default WriteScreen;
